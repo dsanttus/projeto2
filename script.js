059258794678cae6cf9aad25d632598896787260
dsanttus <santos.danielsoares@gmail.com>
2024-04-24 18:39:32 -0300

    foram feitas algumas alterações no código js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Capturando os elementos do DOM
-var titulo = document.getElementById('titulo');
-var listaNaoOrdenada = document.querySelector('ul');
-var link = document.getElementById('link');
-var listaOrdenada = document.getElementById('lista-ordenada');
+let titulo = document.getElementById('titulo');
+let listaNaoOrdenada = document.querySelector('ul');
+let link = document.getElementById('link');
+let listaOrdenada = document.getElementById('lista-ordenada');
 
-// Adicionando conteúdo aos elementos usando .innerText e .innerHTML
 titulo.innerText = 'Título da Página';
 link.innerText = 'Link para Proz Educação';
-listaNaoOrdenada.innerHTML = '<li>Item 1</li><li>Item 2</li><li>Item 3</li>';
+listaNaoOrdenada.innerHTML = '<li>Proz</li><li>Educação</li><li>JOY</li>';
 listaOrdenada.innerHTML = '<li><a href="https://google.com">Acesse o Google</a></li><li><a href="https://facebook.com">Acesse o Facebook</a></li><li><a href="https://instagram.com">Acesse o Instagram</a></li>';
