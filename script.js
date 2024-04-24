78cca2e6576cabcc3e9e35749143387cece39996
dsanttus <santos.danielsoares@gmail.com>
2024-04-24 18:41:32 -0300

    alterado o título da página

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let listaNaoOrdenada = document.querySelector('ul');
 let link = document.getElementById('link');
 let listaOrdenada = document.getElementById('lista-ordenada');
 
-titulo.innerText = 'Título da Página';
+titulo.innerText = 'Projeto 2';
 link.innerText = 'Link para Proz Educação';
 listaNaoOrdenada.innerHTML = '<li>Proz</li><li>Educação</li><li>JOY</li>';
 listaOrdenada.innerHTML = '<li><a href="https://google.com">Acesse o Google</a></li><li><a href="https://facebook.com">Acesse o Facebook</a></li><li><a href="https://instagram.com">Acesse o Instagram</a></li>';
